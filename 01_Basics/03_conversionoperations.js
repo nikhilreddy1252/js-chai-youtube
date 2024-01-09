f7396c9a41c7305c8b929d7d23a22425f80dfd37
Nikhil Reddy <79037191+nikhilreddy1252@users.noreply.github.com>
2024-01-09 22:21:29 +0530

    operations
    operations with types

--- 01_Basics/03_conversionoperations.js
+++ 01_Basics/03_conversionoperations.js
@@ -1,15 +1,15 @@
-let score = true
+let score = "33gh"
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber)
+//console.log(typeof valueInNumber)
 console.log(valueInNumber) // Nan
 
 // "33" => 33 
 // "33ab" => Nan
 // true => 1 false => 0
 
-let isLoggedIn = "1"
+let isLoggedIn = "0"
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(typeof booleanIsLoggedIn)
+//console.log(typeof booleanIsLoggedIn)
 console.log(booleanIsLoggedIn)
 
 // 1 => true , 0 => false 
@@ -18,5 +18,31 @@ console.log(booleanIsLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(typeof stringNumber)
-console.log(stringNumber)
+//console.log(typeof stringNumber)
+//console.log(stringNumber)
+
+// ##################### OPERATIONS ########################
+let value = 3
+let negValue = -value
+//console.log(negValue)
+//console.log(typeof negValue)
+
+//console.log(2+2)
+//console.log(2-2)
+//console.log(2*2)
+//console.log(3/2)
+//console.log(3%2)
+//console.log(3**2)
+
+let str1 = "Hello"
+let str2 = " Nikhil"
+let str3 = str1 + str2;
+console.log(str3)
+
+console.log( 1 + "2")
+console.log( "1" + 2)
+console.log( "1" + "2")
+console.log( "1" + 2 + 4)
+console.log( 1 + "2" + 4)
+console.log( 1 + 2 + "4")// 34
+
