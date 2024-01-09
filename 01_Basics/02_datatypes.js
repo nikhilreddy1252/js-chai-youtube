c4a737bd0b9518f6cc77cc2bc5f0bebededf8b4f
Nikhil Reddy <79037191+nikhilreddy1252@users.noreply.github.com>
2024-01-09 09:50:06 +0530

    02_datatypes
    data types

---
+++ 01_Basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict";// treat all JS code as newer version
+
+// alert(3+3) we are using node js , not browser
+
+console.log(3 +
+     3) // code redability should be high
+console.log("Nikhil Reddy")
+
+let name = "nikhil"
+let age = 28
+let isLoggedIn = false
+
+// number => 2 power 53 range if its too big use bigint
+// bigint
+// string => " " or ' '
+// boolean => true/false
+//null => standalone value 
+//undefined => let state;
+// symbol => unique 
+
+// object 
+
+console.log(typeof "Hitesh") // string 
+console.log(typeof age) //number 
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
