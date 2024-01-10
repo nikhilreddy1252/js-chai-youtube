c7f7d4d305e11d82c037f9fd33d06d4d20595abe
Nikhil Reddy <79037191+nikhilreddy1252@users.noreply.github.com>
2024-01-10 09:32:03 +0530

    comparisions

---
+++ 01_Basics/04_comparision.js
@@ -0,0 +1,20 @@
+// console.log( 2 > 1)
+// console.log( 2 < 1)
+// console.log( 2 >= 1)
+// console.log( 2 <= 1)
+// console.log( 2 == 1)
+// console.log( 2 != 1)
+
+// string literals converted by imploicitly to number
+// console.log("Nikhil" != 1);// Nan != 1 true
+// console.log("02" > 1); // true
+// console.log("02" == 2); // true
+
+console.log( null != 0) // true equality check and comparisions are different 
+
+console.log("2" == 2) // true when we use double equal strings converts to number 
+console.log("2" === 2) // false - it will not convert to number here "strict check"
+
+
+
+
